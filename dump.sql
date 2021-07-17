0f8fffb756dd582cb87e06da561c5f4e432fe957
root <root@ip-172-31-12-27.us-east-2.compute.internal>
2021-07-17 02:37:10 +0000

    adding files edited

---
+++ dump.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS AUTOMATION(name varchar(255), address varchar(255));
+INSERT INTO AUTOMATION values('user1', 'Boston');
+INSERT INTO AUTOMATION values('user2', 'California');
+INSERT INTO AUTOMATION values('user3', 'Arizona');
---
+++ outputsql.sh
@@ -0,0 +1,4 @@
+##echo "select * from AUTOMATION limit 3" | mysql -uvarshini -pvarshini assignment4
+
+echo "select * from AUTOMATION limit 3" | mysql -uvarshini -pvarshini assignment4 >> /home/ec2-user/output
+
